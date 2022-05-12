646fd8ab8696115a7977dc9a986be99911568910
Kaelyn <kaelynhyson@yahoo.com>
2022-05-12 00:10:36 -0700

    Added /ngx to api.js file

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js/ngx';
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
