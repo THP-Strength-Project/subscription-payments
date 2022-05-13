cade735e9f7351f358db1faf5b6dcafcc4e9803d
douglassmoss <douglassmoss@icloud.com>
2022-05-13 13:55:53 -0700

    added example query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,47 @@
-export default function helloWorld({content}) {
+export default function helloWorld({ content }) {
   return (
     <div>
       <h1>Hello world</h1>
-      {content.blogPosts[0].excerpt}
+      <div>{content.blogPosts[0].title}</div>
     </div>
   );
 }
-
 
 export async function getStaticProps() {
   const query = `
     query MyQuery {
       blogPosts {
+        title
         content
         category
         createdAt
         excerpt
       }
     }
-  `
-  
-  const result = await fetch('graphmcmsurl/com', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: JSON.stringify({
-      query,
-   
-    })
-  })
-  
-  const data = await result.json()
-  
-  
+  `;
+
+  const result = await fetch(
+    'https://api-us-west-2.graphcms.com/v2/cl34s61o5647a01xq8jpl51c6/master',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        query
+      })
+    }
+  );
+
+  const data = await result.json();
+  console.log(data.data);
+
   return {
     props: {
-      content: data
+      content: data.data
     },
-    
+
     revalidate: 10
-  }
+  };
 }
