dc5e5037fdb167033b86cbeac27ec907810f5651
Douglass 'Dexter' Moss <douglassmoss@icloud.com>
2022-05-13 13:09:14 -0700

    Update index with graphcms example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,45 @@
-export default function helloWorld() {
+export default function helloWorld({content}) {
   return (
     <div>
       <h1>Hello world</h1>
+      {content.blogPosts[0].excerpt}
     </div>
   );
+}
+
+
+export async function getStaticProps() {
+  const query = `
+    query MyQuery {
+      blogPosts {
+        content
+        category
+        createdAt
+        excerpt
+      }
+    }
+  `
+  
+  const result = await fetch('graphmcmsurl/com', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+   
+    })
+  })
+  
+  const data = await result.json()
+  
+  
+  return {
+    props: {
+      content: data
+    },
+    
+    revalidate: 10
+  }
 }
