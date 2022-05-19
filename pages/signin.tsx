a990091e9320432a39ee064145f49729fa41d734
Kaelyn <kaelynhyson@yahoo.com>
2022-05-18 22:36:58 -0700

    Created signin form and tested it with a user who has already been created, it works

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,5 +1,48 @@
+import { TextInput, Button, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { post } from '@/utils/api';
+
 const SignIn = () => {
-  return 'Sign in';
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email')
+      // password: (value) => '^(?=.*[a-z])(?=.*[A-Z](?=.{6,})'
+    }
+  });
+
+  const signIn = async ({ email, password }) => {
+    alert('hello');
+    const result = await post('/signin', { email, password });
+    console.log(result);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 300 }} mx="auto">
+      <form onSubmit={form.onSubmit(signIn)}>
+        <TextInput
+          required
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps('email')}
+        />
+        <TextInput
+          required
+          label="Password"
+          placeholder="Password"
+          {...form.getInputProps('password')}
+        />
+
+        <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Box>
+  );
 };
 
 export default SignIn;
